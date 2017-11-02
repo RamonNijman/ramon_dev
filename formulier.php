05a95bb365f54833ca9cff1ef546c0536cad8d27
RamonNijman <r.nijman01@student.alfa-college.nl>
2017-11-02 15:56:30 +0100

    formulier.php toegevoegd en extra functies

---
+++ formulier.php
@@ -0,0 +1,7 @@
+<?php
+    $kleur1 = $_POST["kleur1"];
+    $kleur2 = $_POST["kleur2"];
+
+    echo "<span style='color:$kleur1;'>Dit is de kleur: $kleur1</span><br>";
+    echo "<span style='color:$kleur2;'>Dit is de kleur: $kleur2</span>";
+?>
